324d9ddd4821f697b639679ab6d6a66bee33450c
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-07-28 17:08:43 +0530

    second commit

--- src/controller/SampleController.java
+++ src/controller/SampleController.java
@@ -2,15 +2,16 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class SampleController {
     public Label lblTitle;
+    public TextField txtUserName;
+    public TextField txtPassword;
 
     public void btnSignInOnAction(ActionEvent actionEvent) {
-        if (lblTitle.getText().equals("Hello World")) {
-            lblTitle.setText("Hii Jason Momoa");
-        } else {
-            lblTitle.setText("Hello World");
+        if (!txtUserName.equals("") || txtPassword.equals("")) {
+            System.out.println(txtUserName.getText() + " " + txtPassword.getText());
         }
     }
 }
